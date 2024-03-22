1775d6f8a3dfa5a3377f4f0293e178de37827b42
Sourav <ssourav474474@gmail.com>
2024-03-22 18:43:19 +0530

    React Portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,361 @@
+export const Bio = {
+  name: "Sourav Kumar Singh",
+  roles: [
+    "Full Stack Developer",
+    "Learner :)",
+    "Programmer",
+  ],
+  description:
+    "I am an adept learner with a passion for delving into the depths of cutting-edge technology. My areas of expertise include Cloud Technology and Java Full Stack Development, where I've honed my skills to create innovative solutions and user- friendly interfaces.",
+  github: "https://github.com/SouravKumarSingh0",
+  resume:
+    "https://drive.google.com/file/d/1Tsr77NKjnJrEvnhKmQnZ7_EMDsHZi2wy/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/sourav-kumar-singh-10a13b1ab/",
+  facebook: "https://www.facebook.com/souravsingh24"
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+
+  {
+    title: "Framework",
+    skills: [
+      {
+        name: "Spring Boot",
+        image:
+          "https://dz2cdn1.dzone.com/storage/temp/12434118-spring-boot-logo.png",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/flask-logo-icon.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+
+  {
+    title: "DevOps",
+    skills: [
+      {
+        name: "AWS",
+        image:
+          "https://download.logo.wine/logo/Amazon_Web_Services/Amazon_Web_Services-Logo.wine.png",
+      },
+      {
+        name: "Google Cloud Platform",
+        image:
+          "https://static-00.iconduck.com/assets.00/google-cloud-platform-logo-icon-2048x1824-pg4wzspq.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "Kubernetes",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/00/Kubernetes_%28container_engine%29.png",
+      },
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Tenserflow",
+        image:
+          "https://static-00.iconduck.com/assets.00/tensorflow-icon-1911x2048-1m2s54vn.png",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://www.kodnest.com/wp-content/uploads/2022/07/icon_bb-removebg-preview-1-1-150x150.png",
+    role: "Software Developer Intern",
+    company: "Kodnest Technologies",
+    date: "Aug 2023 - Feb 2024",
+    desc: "Completed my Internship at KodNest Technologies, where I gained hands-on expertise in Java Full Stack and Frontend Technologies. I actively engaged in problem-solving, learning directly from industry professionals, and documented my progress on GitHub.",
+    skills: [
+      "Java",
+      "Python",
+      "MySQL",
+      "HTML CSS",
+      "JavaScript",
+      "SpringBoot",
+      "React Js",
+    ],
+    // doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 2,
+    img: "https://static-00.iconduck.com/assets.00/google-cloud-platform-logo-icon-2048x1824-pg4wzspq.png",
+    role: "Google Cloud  Ready Facilitator",
+    company: "Google Cloud Platform",
+    date: "Apr 2022 - Jun 2022",
+    desc: "I completed the Google Cloud Ready Facilitator Program up to the 3rd milestone, engaging in labs such as Engineer Data in Google Cloud, Build Interactive Apps with Google Assistant, Deploy to Kubernetes in Google Cloud, OK Google: Build Interactive Apps with Google Assistant, Create ML Models with BigQuery ML, Serverless Firebase Development, and more. Achieving Silver League status, I earned 21,800 points through successful lab completions within 3 months.",
+  },
+  {
+    id: 3,
+    img: "https://static-00.iconduck.com/assets.00/google-cloud-platform-logo-icon-2048x1824-pg4wzspq.png",
+    role: "30 Days of Google Cloud Program",
+    company: "Google Cloud Platform",
+    date: "Apr 2021 - May 2021",
+    desc: "I completed a 30-day journey on the Google Cloud platform, engaging in a comprehensive program that included immersive hands-on labs covering website building, baseline AI, ML, and BigQuery essentials for data analysis. Achieving Silver League status, I earned 13,700 points through successful lab completions.",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReT61F1bXmP8rmybGQVeoBy7U_0UE_4fv6Ioabs6r8Tj7Tzu6PYXOUM8CXkDXgmH2hZfk&usqp=CAU",
+    school: "Shri Shankaracharya Technical Campus, Bhilai",
+    date: "July 2019 - July 2023",
+    grade: "8.12 CGPA",
+    desc: "I completed my Bachelor's degree in Information Technology from Shri Shankaracharya Technical Campus, Bhilai. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bachelor of Technology - BTech, Information Technology",
+  },
+  {
+    id: 1,
+    img: "https://davpschirimiri.com/MyImages/LOGO.png",
+    school: "D.A.V Public School, Kota",
+    date: "Apr 2016 - Apr 2019",
+    grade: "54.2%",
+    desc: "I completed my class 12 high school education at D.A.V Public School, Kota, where I studied PCM with Computer Science.",
+    degree: "CBSE(XII), PCM",
+  },
+  {
+    id: 2,
+    img: "https://davpschirimiri.com/MyImages/LOGO.png",
+    school: "D.A.V Public School, Chirimiri",
+    date: "2004 - 2016",
+    grade: "8.6 CGPA",
+    desc: "I completed my class 10 education at D.A.V Public School, Chirimiri, where I studied Science, Maths, Social Science with Hindi.",
+    degree: "CBSE(X), Science",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Note Website",
+    date: "Jun 2022 - Jul 2022",
+    description:
+      "A Simple Note Website where Users can create an account and then once they Signed in they are able to Add notes or make New Notes at same time. Then they can Delete those Notes. Users can also log back in on any Computer they want and access those Notes.",
+    image:
+      "https://simplenoteblog.files.wordpress.com/2020/07/customizer-2.png?w=1024",
+    tags: ["Python", "HTML", "Flask", "MySQL", "Bootstrap"],
+    category: "web app",
+    github: "https://github.com/SouravKumarSingh0/Note-Website",
+    //webapp: "https://github.com/SouravKumarSingh0/Note-Website",
+  },
+  {
+    id: 1,
+    title: "TODO list",
+    date: "Jan 2022 - Jan 2022",
+    description:
+      "This project is a simple TODO list web application. Users can add tasks using the input field and the + button. Each task item can be edited or removed individually. The design features a clean interface with a background image and responsive layout.",
+    image:
+      "https://1.bp.blogspot.com/-i78iKr_P9Pk/X9ohjXyc5eI/AAAAAAAAA_4/8UauxZaOgUshGK7MXwW1gZqts7Zrf_AewCLcBGAsYHQ/s1280/Todo%2BList%2BApp%2Busing%2BHTML%2BCSS%2B%2526%2BJavaScript.webp",
+    tags: ["HTML", "CSS", "JAVASCRIPT"],
+    category: "web app",
+    github: "https://github.com/SouravKumarSingh0/todo",
+    webapp: "https://souravkumarsingh0.github.io/todo/",
+  },
+
+  {
+    id: 2,
+    title: "Git-Search-Profile",
+    description:
+      "Designed a website that allows users to find relevant information about GitHub users by entering their usernames. Implemented responsive design for compatibility across all devices.",
+    image:
+      "https://www.iakademi.com/wp-content/uploads/2021/06/git-github-nedir-1.png",
+    tags: ["HTML", "CSS", "JAVASCRIPT", "Rest API"],
+    category: "web app",
+    github: "https://github.com/SouravKumarSingh0/Git-Search-Profile",
+    webapp: "https://souravkumarsingh0.github.io/Git-Search-Profile/",
+  },
+
+  //id: 2,
+  //title: "",
+  // date: "",
+  // description:
+  //  "",
+  //image:
+  //  "https:",
+  // tags: ["Python"],
+  //category: "",
+  // github: "https:",
+  //webapp: "https:",
+  //member: [
+  // {
+  //  name: "",
+  //  img: "https:",
+  // linkedin: "",
+  // github: "",
+  // },
+  //{
+  //  name: "",
+  // img: "https:",
+  // linkedin: "",
+  // github: "",
+  // },
+  //],
+  {
+    id: 3,
+    title: "Face Recognition Attendance System",
+    description:
+      "This Python project utilizes OpenCV for face recognition. It begins by training a LBPH (Local Binary Patterns Histograms) face recognizer on a dataset of labeled face images. Then, it continuously captures video frames, detects faces using a Haar cascade classifier, and predicts the identities of the detected faces using the trained recognizer, displaying the results in real-time.",
+    image: "https://repository-images.githubusercontent.com/272034449/90e10e00-b308-11ea-9e64-3cc1cd32f799",
+    tags: ["Python", "OpenCV", "NumPy"],
+    category: "Others",
+    github:
+      "https://github.com/SouravKumarSingh0/Face-Recognition-Attendance-System/tree/main",
+    //webapp: "https://github.com/SouravKumarSingh0/Face-Recognition-Attendance-System/tree/main",
+  },
+  {
+    id: 10,
+    title: "Password Generator & Manager",
+    date: "Jun 2023 - Jul 2023",
+    description:
+      "This is a Password Generator and Management System. The Password Generation module is responsible for helping you create unique and secure password. The Password Management module is responsible to store and retrieve the passwords pertaining to the websites in which you want to have an account.",
+    image:
+      "https://miro.medium.com/v2/resize:fit:1400/1*Mwu3L3OnJHQDj0--V6sGIw.jpeg",
+    tags: ["Python", "Tkinter GUI Toolkit", "MySQL"],
+    category: "Others",
+    github:
+      "https://github.com/SouravKumarSingh0/Password-Generator-and-Manager",
+    //webapp: "",
+  },
+  {
+    id: 4,
+    title: "Quiz App",
+    description:
+      "The Quiz App, developed with React, offers users an interactive platform to engage in quizzes spanning diverse topics. With multiple-choice questions and a built-in timer, users can test their knowledge under time constraints. The app boasts a dynamic progress bar, ensuring users are aware of the time remaining for each question. Its responsive design caters to users across various devices, facilitating seamless quiz-taking experiences.",
+    image:
+      "https://user-images.githubusercontent.com/26218291/133905987-58c5b658-f3f8-45de-ba1e-dcaae04d600b.png",
+    tags: ["React Js", "HTML", "CSS", "SCSS"],
+    category: "web app",
+    github: "https://github.com/SouravKumarSingh0/Quiz-App",
+    webapp: "https://sourav-quiz-app.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "Digital Clock",
+    description:
+      "This project is a simple digital clock web application. It displays the current time in hours, minutes, and seconds, along with the corresponding period (AM or PM). The design features a clean layout with bold text on a gradient background. The clock updates every second to reflect the current time accurately.",
+    image:
+      "https://images.prismic.io/screencloud/fe15c23f-253e-4b4f-b4b9-4db6387f5ba9_Clock+1.png?auto=compress,format",
+    tags: ["HTML", "CSS", "JavaScript"],
+    category: "web app",
+    github: "https://github.com/SouravKumarSingh0/Digital-Clock",
+    webapp: "https://souravkumarsingh0.github.io/Digital-Clock/",
+  },
+  {
+    id: 6,
+    title: "Weather App",
+    description:
+      "This project is a weather dashboard app with search functionality to find current weather conditions and forecasted weather for multiple cities. It uses the OpenWeather API to retrieve data for the cities, AJAX to hook into the API to retrieve the data in JSON format, dynamically updated HTML and CSS powered by jQuery, and displays a search history which the user can click to access past cities searched via local storage.",
+    image:
+      "https://s3-alpha.figma.com/hub/file/2947805380/40c12560-d19c-4439-821e-7eef49dd1826-cover.png",
+    tags: ["HTML", "Rest API", "CSS", "Bootstrap", "JavaScript", "Jquery"],
+    category: "web app",
+    github: "https://github.com/SouravKumarSingh0/Weather-Dashboard",
+    webapp: "https://souravkumarsingh0.github.io/Weather-Dashboard/",
+  },
+  {
+    id: 8,
+    title: "Object Detection Application",
+    //date: "Jan 2021",
+    description:
+      "Created an application using TensorFlow and Google Cloud Platform (GCP) to detect multiple objects in an uploaded image. Utilized Pre-trained object detection models and Object detection APIs.",
+    image:
+      "https://i.ytimg.com/vi/oRWxrN-DONg/maxresdefault.jpg",
+    tags: ["Python", "Google Cloud Platform", "TensorFlow", "Flask", "API"],
+    category: "Cloud",
+    github:
+      "https://github.com/SouravKumarSingh0/Object-Detection-Web-Application",
+    //webapp: "https://github.com/SouravKumarSingh0/Object-Detection-Web-Application",
+  },
+];
+
