12b6c8a4056ee875826398a8fd3d7865d99ffa70
Sourav <ssourav474474@gmail.com>
2024-07-16 13:16:29 +0530

    resume update

--- src/data/constants.js
+++ src/data/constants.js
@@ -165,7 +165,7 @@ export const experiences = [
       "SpringBoot",
       "React Js",
     ],
-    // doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+     doc: "https://drive.google.com/file/d/1KEu0zyxO6MSw5ndzcP3ru-Pgx2UdH8mu/view?usp=sharing",
   },
   {
     id: 2,
